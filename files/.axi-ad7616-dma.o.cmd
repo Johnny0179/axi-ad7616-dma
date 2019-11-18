12a380c49f12994761f2dd6b5bf7e84062793598
Johnny0179 <songjunlin0179@163.com>
2019-11-18 20:58:04 +0800

    can't enter the init function

--- files/.axi-ad7616-dma.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/johnny/work/linux_drivers/axi-ad7616-dma/files/axi-ad7616-dma.o := arm-linux-gnueabihf-ld  -EL    -r -o /home/johnny/work/linux_drivers/axi-ad7616-dma/files/axi-ad7616-dma.o /home/johnny/work/linux_drivers/axi-ad7616-dma/files/ad7616.o /home/johnny/work/linux_drivers/axi-ad7616-dma/files/spi_engine_linux.o 
--- files/.tmp_versions/axi-ad7616-dma.mod
+++
@@ -1,3 +0,0 @@
-/home/johnny/work/linux_drivers/axi-ad7616-dma/files/axi-ad7616-dma.ko
-/home/johnny/work/linux_drivers/axi-ad7616-dma/files/ad7616.o /home/johnny/work/linux_drivers/axi-ad7616-dma/files/spi_engine_linux.o
-
